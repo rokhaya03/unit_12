293406bade952e618eec7889d9b68ba0fd67e5f3
rokhaya03 <rdiagne@student.cscc.edu>
2025-11-20 14:54:25 -0500

    p2-modified created fleet to also create all the columns

--- alien_fleet.py
+++ alien_fleet.py
@@ -1,49 +1,75 @@
 import pygame
-from alien import Alien
 from typing import TYPE_CHECKING
-
-
-
+from alien import Alien
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
+    
+
 
 class AlienFleet:
-    def __init__(self,game:"AlienInvasion"):
-        self.game=game
-        self.settings=game.settings
-        self.fleet=pygame.sprite.Group()
-        self.fleet_direction=self.settings.fleet_direction
-        self.fleet_drop_speed=self.settings.fleet_drop_speed
+
+    def __init__(self, game: 'AlienInvasion'):
+        self.game = game
+        self.settings = game.settings
+        self.fleet = pygame.sprite.Group()
+        self.fleet_direction = self.settings.fleet_direction
+        self.fleet_droop_speed = self.settings.fleet_drop_speed
 
         self.create_fleet()
     
+
     def create_fleet(self):
-        alien_h=self.settings.alien_h
-        screen_h=self.settings.screen_h
+        alien_w = self.settings.alien_w
+        alien_h = self.settings.alien_h
+        screen_w = self.settings.screen_w
+        screen_h = self.settings.screen_h
 
-        fleet_h= self.calculate_fleet_size(alien_h, screen_h)
-        fleet_vertical_space= fleet_h * alien_h
-        y_offset=int((screen_h-fleet_vertical_space)*2)
 
-        for row in range(fleet_h):
-            current_y=(alien_h * row) + y_offset
-            if row % 2 ==0:
-                continue
-            self.create_alien(10,current_y)
+        fleet_w, fleet_h =  self.calculate_fleet_size(alien_w, screen_w, alien_h, screen_h)
 
-    def calculate_fleet_size(self,alien_h, screen_h):
-        fleet_h=(screen_h//alien_h)
- 
-        if fleet_h % 2==0:
-            fleet_h-=1
+        x_offset, y_offset = self.calculate_offsets(alien_w, alien_h, screen_w, fleet_w, fleet_h)
+
+        self._create_rectangle_fleet(alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset)
+
+    def _create_rectangle_fleet(self, alien_w, alien_h, fleet_w, fleet_h, x_offset, y_offset):
+        for col in range(fleet_h):
+            for row in range(fleet_w):
+                current_x = alien_w * row + x_offset
+                current_y = alien_h * col + y_offset
+                if col % 2 == 0 or row %2== 0:
+                    continue
+                self._create_alien( current_x, current_y)
+
+    def calculate_offsets(self, alien_w, alien_h, screen_h, fleet_w, fleet_h):
+        half_screen = self.settings.screen_w//2
+        fleet_horizontal_space = fleet_w * alien_w
+        fleet_vertical_space = fleet_h * alien_h
+        x_offset = int((half_screen - fleet_horizontal_space )//2)
+        y_offset = int((screen_h -fleet_vertical_space)//2)
+        return x_offset,y_offset
+
+
+    def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h):
+        fleet_h = (screen_h*alien_h)
+        fleet_w = ((screen_w /2)//alien_w)
+
+        if fleet_w % 2 == 0:
+            fleet_w -= 1
         else:
-            fleet_h-=2
-        return fleet_h
-    def create_alien(self, current_x:int, current_y:int):
-        new_alien=Alien(self, current_x, current_y)
+            fleet_w -= 2
+        if fleet_h % 2 == 0:
+            fleet_h -= 1
+        else:
+            fleet_h -=2
+        
+        return int(fleet_w), int(fleet_h)
+
+    def _create_alien(self, current_x: int, current_y: int):
+        new_alien = Alien(self, current_x, current_y)
         self.fleet.add(new_alien)
 
+    
     def draw(self):
-        alien:'Alien'
+        alien: 'Alien'
         for alien in self.fleet:
             alien.draw_alien()
--- alien_invasion.py
+++ alien_invasion.py
@@ -16,13 +16,13 @@ class AlienInvasion:
         self.settings=Settings()
         
         self.screen=pygame.display.set_mode(
-            (self.settings.screen_w,self.settings.screen_w)
+            (self.settings.screen_w,self.settings.screen_h)
              )
         pygame.display.set_caption(self.settings.name)
 
         self.bg= pygame.image.load(self.settings.bg_file)
 
-        self.bg= pygame.transform.scale(self.bg,(self.settings.screen_w, self.settings.screen_w))
+        self.bg= pygame.transform.scale(self.bg,(self.settings.screen_w, self.settings.screen_h))
 
         self.running=True
         self.clock=pygame.time.Clock()

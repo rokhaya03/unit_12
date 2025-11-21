af975d48e1c479568356aafd8cf2131c9bde8014
rokhaya03 <rdiagne@student.cscc.edu>
2025-11-21 15:18:51 -0500

    p2-Finished game status check to only play while there are ships remaining

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,15 +5,18 @@
 import sys
 import pygame
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from arsenal import Arsenal
 from alien_fleet import AlienFleet
+from time import sleep
 
 class AlienInvasion:
   
     def __init__(self):
         pygame.init()
         self.settings=Settings()
+        self.game_stats=GameStats(self.settings.starting_ship_count)
         
         self.screen=pygame.display.set_mode(
             (self.settings.screen_w,self.settings.screen_h)
@@ -39,14 +42,16 @@ class AlienInvasion:
         self.ship=Ship(self, Arsenal(self))
         self.alien_fleet=AlienFleet(self)
         self.alien_fleet.create_fleet()
+        self.game_active=True
 
     def run_game(self):
         
         while self.running:
             self._check_events()
-            self.ship.update()
-            self.alien_fleet.update_fleet()
-            self._check_collisions()
+            if self.game_active:
+                self.ship.update()
+                self.alien_fleet.update_fleet()
+                self._check_collisions()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
 
@@ -54,9 +59,9 @@ class AlienInvasion:
     def _check_collisions(self):
         # Check collisions for ship 
         if self.ship.check_collisions(self.alien_fleet.fleet):
-            self._reset_level()
+            self._check_game_status()
         if self.alien_fleet.check_fleet_right():
-            self._reset_level()
+            self._check_game_status()
 
         collisions = self.alien_fleet.check_collisions(self.ship.arsenal.arsenal)
         if collisions:
@@ -64,9 +69,23 @@ class AlienInvasion:
             self.impact_sound.fadeout(500)
 
 
+            
 
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
+          
+
+    def _check_game_status(self):
+        #Checks to see if the player is still alive to see if damage is taken
+        
+        if self.game_stats.ships_left > 0:
+            self.game_stats.ships_left-= 1
+            self._reset_level()
+            sleep(0.5)
+        else:
+            self.game_active=False
+         
+            
 
     def _reset_level(self):
         #Resets the everything in the current level
